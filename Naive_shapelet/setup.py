cebaf5288803a94997138dba27017ca69a1dac34
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2024-01-31 15:18:07 -0700

    Update setup.py

--- Naive_shapelet/setup.py
+++ Naive_shapelet/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='naive-shapelet-CF',
+    name='naive_shapelet_CF',
     version='0.1.1',
     packages=find_packages(),
     install_requires=[
