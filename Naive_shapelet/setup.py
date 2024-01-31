7a7b7bcbea41ae249bc0f0a6fd0537cf601d0ef2
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2024-01-31 15:07:56 -0700

    Update setup.py

--- Naive_shapelet/setup.py
+++ Naive_shapelet/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='naive-shapelet-CF',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pandas',
