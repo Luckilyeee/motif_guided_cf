6d1f77d9e5370434ec4acd6e5dd022170985953d
lipeiyu <60626754+Luckilyeee@users.noreply.github.com>
2024-01-31 13:13:28 -0700

    Create setup.py

---
+++ Naive_shapelet/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='naive-shapelet-CF',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        pandas,
+        numpy,
+        tslearn,
+        pyts,
+        sklearn,
+        matplotlib,
+        scipy
+    ],
+    author='Peiyu Li',
+    author_email='peiyu.li@usu.edu',
+)
